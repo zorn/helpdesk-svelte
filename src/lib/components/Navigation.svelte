b569778d35c1c63fa8cba625876642e6e99bc74e
Mike Zornek <zorn@zornlabs.com>
2023-08-07 15:07:39 -0400

    Create a new tickets index page, linked in the navigation.

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -8,6 +8,7 @@
 
 <nav class="list-nav p-4">
 	<ul>
+		<li><a href="/tickets" on:click={drawerClose}>Tickets</a></li>
 		<li><a href="/" on:click={drawerClose}>Notes</a></li>
 		<li><a href="/?test=1" on:click={drawerClose}>Other Page 1</a></li>
 		<li><a href="/?test=2" on:click={drawerClose}>Other Page 2</a></li>
---
+++ src/routes/tickets/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+
+	const sourceData = [
+		{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+		{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+		{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+		{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' }
+	];
+
+	const tableSimple = {
+		// A list of heading labels.
+		head: ['Name', 'Symbol', 'Weight'],
+		// The data visibly shown in your table body UI.
+		body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
+		// Optional: The data returned when interactive is enabled and a row is clicked.
+		meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
+		// Optional: A list of footer labels.
+		foot: ['Total', '', '<code class="code">5</code>']
+	};
+
+	function mySelectionHandler(event) {
+		console.log(event.detail);
+	}
+</script>
+
+<h2 class="h2 mb-10">Tickets</h2>
+
+<Table source={tableSimple} interactive={true} on:selected={mySelectionHandler} />
